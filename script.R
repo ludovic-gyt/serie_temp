331056df246248f7780e0823d91dc2a17700a2b9
ludovic-gyt <ludovic-52@hotmail.fr>
2023-04-01 13:11:57 +0200

    sauvegarde

--- script.R
+++ script.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 library(zoo) #format de serie temporelle pratique et facile d'utilisation (mais plus volumineux)
 library(tseries) #diverses fonctions sur les series temporelles
 
@@ -12,14 +13,33 @@ list.files() #liste les elements du wd
 datafile <- "input/valeurs_serie.csv" #definit le fichier de donnees
 
 data <- read.csv(datafile,sep = ";") #importe un fichier .csv dans un objet de classe data.frame
+data <- data[nrow(data):1,]
 
-xm.source <- zoo(data[[1]]) # convertit le premiers element de data en serie temporelle de type "zoo"
+# Convertir la colonne "date" en format date
+
+
+
+#Convertir les valeurs en zoo
+xm.source <- zoo(data[[2]])# convertit le premiers element de data en serie temporelle de type "zoo"
 T <- length(xm.source)
 xm <- xm.source[1:(T-4)] #supprime les 4 dernieres valeurs
 
-plot(xm)
 
+
+####Graphique####
 plot(xm, xaxt="n")
-axis(side=1,at=seq(0,196,12))
-acf(xm)
+
+
+####autocorrélation####
+par(mfrow=c(1,2))
+acf(xm); pacf(xm)
+dev.off()
+####detrend#####
+
+y <- xm - mean(xm)
+plot(xm)
+plot(y)
+par(mfrow=c(1,2)) 
+acf(y,30);pacf(y,30) 
+
 
