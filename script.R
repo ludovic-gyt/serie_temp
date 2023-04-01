2bcb8f8d1a84fc50e3c62f92456bec2dcaf9bfcd
ludovic-gyt <ludovic-52@hotmail.fr>
2023-04-01 12:26:42 +0200

    ajout

--- script.R
+++ script.R
@@ -1,20 +1,25 @@
 library(zoo) #format de serie temporelle pratique et facile d'utilisation (mais plus volumineux)
 library(tseries) #diverses fonctions sur les series temporelles
 
-#set-up
-path <- "/Users/ludovic/Desktop/ENSAE/S2/series temporelles/serie_temp"
+####set-up####
+path <- "/Users/ludovic/Desktop/ENSAE/S2/series/serie_temp"
 setwd(path) #definit l'espace de travail (working directory ou "wd")
 getwd() #affiche le wd
 list.files() #liste les elements du wd
 
-#import
+####import####
 
-datafile <- "input/valeurs_mensuelles.csv" #definit le fichier de donnees
+datafile <- "input/valeurs_serie.csv" #definit le fichier de donnees
 
-data <- read.csv(datafile,sep=";") #importe un fichier .csv dans un objet de classe data.frame
+data <- read.csv(datafile,sep = ";") #importe un fichier .csv dans un objet de classe data.frame
 
-xm.source <- zoo(data[[1]]) #convertit le premier element de data en serie temporelle de type "zoo"
+xm.source <- zoo(data[[1]]) # convertit le premiers element de data en serie temporelle de type "zoo"
 T <- length(xm.source)
 xm <- xm.source[1:(T-4)] #supprime les 4 dernieres valeurs
 
+plot(xm)
+
+plot(xm, xaxt="n")
+axis(side=1,at=seq(0,196,12))
+acf(xm)
 
